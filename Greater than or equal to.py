282fd9f98eab285b5f0c6bf856e9973fddfc825c
Nadhir <dndnthedev@gmail.com>
2026-08-04 03:49:08 +0100

    more
    a program which asks for two integer numbers. The program should then print out whichever is greater. If the numbers are equal, the program should print a different message.

---
+++ Greater than or equal to.py
@@ -0,0 +1,8 @@
+fnum = int(input("Please type in the first number: "))
+snum = int(input("Please type in the another number: "))
+if fnum > snum :
+    print(f"The greater number was: {fnum}")
+elif snum > fnum : 
+    print(f"The greater number was: {snum}")
+else : 
+    print("The numbers are equal!")
