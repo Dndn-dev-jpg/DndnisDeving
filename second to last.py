595e9e8f286efc56a6f41e3e86ebc14d1b32ee3d
Nadhir <dndnthedev@gmail.com>
2026-08-09 06:09:43 +0100

    Second To Last in string

---
+++ second to last.py
@@ -0,0 +1,7 @@
+string = str(input("Please type in a string: "))
+L = string[1]
+T = string [-2]
+if L == T :
+    print(f"The second and the second to last characters are {L}")
+else : 
+    print("The second and the second to last characters are different")
