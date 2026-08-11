0ddd325cf37a3b8da68c487854bce9bf3d0c8d94
Nadhir <dndnthedev@gmail.com>
2026-08-11 23:01:25 +0100

    First letters of words in a sentence
    a program which asks the user to type in a sentence. The program then prints out the first letter of each word in the sentence, each letter on a separate line.
    
    An example of expected behaviour:
    Please type in a sentence: Humpty Dumpty sat on a wall
    H
    D
    s
    o
    a
    w

---
+++ First letters of words.py
@@ -0,0 +1,7 @@
+sentence = str(input("Please type in a sentence: "))
+while True :
+    print(sentence[0])
+    index = sentence.find(" ")
+    if index == -1  :
+        break
+    sentence = sentence[index + 1 : ]
