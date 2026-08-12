0d44907ee02a5a3714e5365c8d8f035875defefa
Nadhir <dndnthedev@gmail.com>
2026-08-12 04:30:31 +0100

    Taking Turns
    a program which asks the user to type in a number. The program then prints out the positive integers between 1 and the number itself, alternating between the two ends of the range as in the examples below.
    Please type in a number: 5
    1
    5
    2
    4
    3
    or 
    Please type in a number: 6
    1
    6
    2
    5
    3
    4

---
+++ Moving inward.py
@@ -0,0 +1,11 @@
+number = int(input("Please type in a number: "))
+left = 1 
+right = number
+while left < right :
+    print(left)
+    print(right)
+    left += 1
+    right -=1
+if left == right : 
+    print(left) 
+#had different variables , i had to change them for better understanding in case anyone opened my code
